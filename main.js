7f7dd0063ad86080e10d9654f06103f911ec9629
LLLida <0adilmohammad0@gmail.com>
2023-05-25 19:27:18 +0300

    майнинг🥰

--- main.js
+++ main.js
@@ -13,15 +13,18 @@ var WebSocket = require("ws");
 var http_port = process.env.HTTP_PORT || 3001;
 var p2p_port = process.env.P2P_PORT || 6001;
 var initialPeers = process.env.PEERS ? process.env.PEERS.split(',') : [];
+var difficulty = 4;
 
 class Block {
 
-    constructor(index, previousHash, timestamp, data, hash) {
+    constructor(index, previousHash, timestamp, data, hash, difficulty, nonce) {
         this.index = index;
         this.previousHash = previousHash.toString();
         this.timestamp = timestamp;
         this.data = data;
         this.hash = hash.toString();
+        this.difficulty = difficulty;
+        this.nonce = nonce;
     }
 
 };
@@ -35,7 +38,8 @@ var MessageType = {
 
 var getGenesisBlock = () => {
     return new Block(0, "0", 2348753243, "Adil's first block",
-        "8d9d5a7ff4a78042ea6737bf59c772f8ed27ef3c9b576eac1976c91aaf48d2de");
+        "8d9d5a7ff4a78042ea6737bf59c772f8ed27ef3c9b576eac1976c91aaf48d2de",
+        0, 0);
 }
 
 var blockchain = [getGenesisBlock()];
@@ -46,7 +50,8 @@ var initHttpServer = () => {
 
     app.get('/blocks', (req, res) => res.send(JSON.stringify(blockchain)));
     app.post('/mineBlock', (req, res) => {
-        var newBlock = generateNextBlock(req.body.data);
+        // var newBlock = generateNextBlock(req.body.data);
+        var newBlock = mineBlock(req.body.data);
         addBlock(newBlock);
         broadcast(responseLatestMsg());
         console.log('block added: ' + JSON.stringify(newBlock));
@@ -143,11 +148,11 @@ var generateNextBlock = (blockData) => {
 };
 
 var calculateHashForBlock = (block) => {
-    return calculateHash(block.index, block.previousHash, block.timestamp, block.data);
+    return calculateHash(block.index, block.previousHash, block.timestamp, block.data, block.nonce);
 };
 
-var calculateHash = (index, previousHash, timestamp, data) => {
-    return CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+var calculateHash = (index, previousHash, timestamp, data, nonce) => {
+    return CryptoJS.SHA256(index + previousHash + timestamp + data + nonce).toString();
 };
 
 var addBlock = (newBlock) => {
@@ -197,6 +202,21 @@ var isValidChain = (blockchainToValidate) => {
     }
     return true;
 };
+
+var mineBlock = (blockData) => {
+    var previousBlock = getLatestBlock();
+    var nextIndex = previousBlock.index + 1;
+    var nonce = 0;
+    var nextTimestamp = new Date().getTime() / 1000;
+    var nextHash = calculateHash(nextIndex, previousBlock.hash, nextTimestamp, blockData, nonce);
+    while (nextHash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+        nonce++;
+        nextTimestamp = new Date().getTime() / 1000;
+        nextHash = calculateHash(nextIndex, previousBlock.hash, nextTimestamp, blockData, nonce);
+        console.log("\"index\":" + nextIndex + ",\"previousHash\":"+previousBlock.hash+ "\"timestamp\":"+nextTimestamp+",\"data\":"+blockData+ ",\x1b[33mhash: " + nextHash + " \x1b[0m," + "\"difficulty\":"+difficulty+ " \x1b[33mnonce: " + nonce + " \x1b[0m ");
+    }
+    return new Block(nextIndex, previousBlock.hash, nextTimestamp, blockData, nextHash, difficulty, nonce);
+}
 
 var getLatestBlock = () => blockchain[blockchain.length - 1];
 var queryChainLengthMsg = () => ({'type': MessageType.QUERY_LATEST});
